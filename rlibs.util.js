d8b9546610257489ce6dd91b03d84d26c8289d65
Rodney Barreto <rwbarreto@gmail.com>
2022-06-28 21:27:42 -0300

    Funções para manipulação de el dropdown

--- rlibs.util.js
+++ rlibs.util.js
@@ -61,7 +61,7 @@ RLIBS.util = (function (app, global) {
    * @return {String} String formatada 'HH:MM' contendo o intervalo.
    */
   _hoursInterval = function (hmIni, hmFim) {
-    var horaFim = new Date(),
+    let horaFim = new Date(),
       horaIni = new Date(),
       milsec = 0,
       interv = 0,
@@ -108,10 +108,138 @@ RLIBS.util = (function (app, global) {
       console.log(e);
     }
   };
-  
+
+  /**
+   * Funcao para inserir option em uma drop-dwon lists.
+   * Recebe, como parametros, o elemento html drop-dwon,
+   * o nome do atributo referente ao valor e o nome do
+   * atributo referente ao texto exibido ao usuario.
+   *
+   * Exemplo:
+   *
+   *   RLIBS.util.inserirOpcaoDropDown(element, value, label);
+   *
+   * @param {Object} element Html que sera alterado.
+   * @param {String} value Nome da propriedade que sera substituida por seu valor.
+   * @param {String} label Nome da propriedade que sera substituida por seu valor.
+   */
+  _insertDropDownListOption = function (element, value, label) {
+    let opt = document.createElement('option');
+    opt.value = value;
+    opt.text = label;
+    try {
+      element.add(opt, null);
+    } catch (e) {
+      // IE
+      element.add(opt);
+    }
+    return this;
+  };
+
+  /**
+   * Funcao para inserir options em uma drop-dwon lists.
+   * Recebe, como parametros, o elemento html drop-dwon,
+   * a lista de objetos contendo os dados que seram inseridos,
+   * o nome do atributo referente ao valor, o nome do
+   * atributo referente ao texto exibido ao usuario e,
+   * por ultimo, um booleano opcional que indicara se o
+   * valor e o texto seram concatenados para exibicao ao usuario.
+   *
+   * Exemplo:
+   *
+   *   RLIBS.util.inserirOpcoesDropDown(element, data, value, label, concat, extraLabel);
+   *
+   * @param {Object} element Html que sera alterado.
+   * @param {Object} data Array de objetos contendo os valores.
+   * @param {String} value Nome da propriedade que ficara dentro do atributo "value" da tag [option].
+   * @param {String} label Nome da propriedade que sera exibida para o usuario.
+   * @param {Boolean} concat Booleano para concatenar a exibicao do value com label separado por um traço (-).
+   * @param {String} extraLabel Nome da propriedade que sera concatenada na exibicao do label ao usuario.
+   */
+  _insertDropDownListOptions = function (
+    element,
+    data,
+    value,
+    label,
+    concat = false,
+    extraLabel = ''
+  ) {
+    let property = null;
+
+    try {
+      if (typeof element === 'undefined' || typeof element === null)
+        throw { message: 'Parâmetro [element] ausente.' };
+
+      if (typeof data === 'undefined' || typeof data === null)
+        throw { message: 'Parâmetro [data] ausente.' };
+
+      if (typeof value === 'undefined' || typeof data === null)
+        throw { message: 'Parâmetro [value] ausente.' };
+
+      if (typeof label === 'undefined' || typeof data === null)
+        throw { message: 'Parâmetro [label] ausente.' };
+
+      for (let i = 0, ii = data.length; i < ii; i += 1) {
+        property = data[i];
+        if (concat) {
+          if (extraLabel === '') {
+            _insertDropDownListOption(
+              element,
+              property[value],
+              property[value] + ' - ' + property[label]
+            );
+          } else {
+            _insertDropDownListOption(
+              element,
+              property[value],
+              property[value] +
+                ' - ' +
+                property[label] +
+                ' - ' +
+                property[extraLabel]
+            );
+          }
+        } else {
+          _insertDropDownListOption(element, property[value], property[label]);
+        }
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
+    return this;
+  };
+
+  /**
+   * Funcao para remover options em uma drop-dwon lists.
+   * Recebe, como parametro, o elemento html drop-dwon
+   * que sera alterado.
+   *
+   * Exemplo:
+   *
+   *   RLIBS.util.removerOpcoesDropDown(element);
+   *
+   * @param {Object} element Html que sera alterado.
+   */
+  _removeDropDownListOptions = function (element) {
+    try {
+      if (typeof element === 'undefined' || typeof element === null)
+        throw { message: 'Parâmetro [element] ausente.' };
+
+      for (let i = element.options.length - 1, ii = 0; i >= ii; i -= 1) {
+        element.remove(i);
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
+    return this;
+  };
+
   // Interface publica
   return {
     mesclarObjetos: _objectsMerge,
     horaIntervalo: _hoursInterval,
+    insertDropDownListOption: _insertDropDownListOption,
+    insertDropDownListOptions: _insertDropDownListOptions,
+    removeDropDownListOptions: _removeDropDownListOptions
   };
 })(RLIBS, this);
